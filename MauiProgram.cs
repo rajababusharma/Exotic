633b5cd3eeecc64e12c0c84bab490c48d4c4b806
rajababusharma <39822786+rajababusharma@users.noreply.github.com>
2023-08-17 09:57:32 +0530

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
+using Exotic.Implementation;
+using Exotic.View;
+using Microsoft.Extensions.Logging;
+
+namespace Exotic;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Services.AddSingleton<IFileSaver>(FileSaver.Default);
+		builder.Services.AddTransient<Warehouse>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
